590275e6fac5dc4b57e3310934a8dc27220bf6ef
Houcem Belkhiria <119082763+houcembelkhiria@users.noreply.github.com>
2024-04-12 16:51:11 +0200

    Update BatchFile.bat

--- BatchFile.bat
+++ BatchFile.bat
@@ -2,7 +2,17 @@
 echo Cleaning the project...
 mvn clean
 
+if errorlevel 1 (
+    echo An error occurred during the cleaning process.
+    exit /b %errorlevel%
+)
+
 echo Running tests using testng.xml...
 mvn test -Dtestng=testng.xml
+
+if errorlevel 1 (
+    echo An error occurred during the test execution.
+    exit /b %errorlevel%
+)
 
 echo Finished.
