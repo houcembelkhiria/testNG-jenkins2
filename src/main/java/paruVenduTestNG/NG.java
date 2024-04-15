1d77032b69ffbc194c776006e5f3006d90a7ff71
Houcem Belkhiria <119082763+houcembelkhiria@users.noreply.github.com>
2024-04-15 12:25:13 +0200

    Update NG.java

--- src/main/java/paruVenduTestNG/NG.java
+++ src/main/java/paruVenduTestNG/NG.java
@@ -18,8 +18,8 @@ public class NG {
         System.out.println("Test NG started !!!! ");
 
 		String projectLocation = System.getProperty("user.dir");
-		System.out.println(projectLocation);
-		System.setProperty("webdriver.gecko.driver","src/lib/geckodriver.exe");
+		System.out.println("location is " + projectLocation);
+		System.setProperty("webdriver.gecko.driver","projectLocation + src/lib/geckodriver.exe");
 		System.setProperty("webdriver.firefox.bin", "C:/Program Files/Mozilla Firefox/firefox.exe");
 		driver = new FirefoxDriver();
 	}
