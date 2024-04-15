0a12352e14bcaa5feaf840f1265a1e2907b8d5df
Houcem Belkhiria <119082763+houcembelkhiria@users.noreply.github.com>
2024-04-15 10:36:31 +0200

    Update NG.java restore

--- src/main/java/paruVenduTestNG/NG.java
+++ src/main/java/paruVenduTestNG/NG.java
@@ -1,5 +1,4 @@
 package paruVenduTestNG;
-import io.github.bonigarcia.wdm.WebDriverManager; // Add this import
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -11,115 +10,117 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class NG {
-    WebDriver driver = null;
-        
+	WebDriver driver = null;
+		
 
-    @BeforeTest
-    public void setUp() {
+	@BeforeTest
+	public void setUp() {
         System.out.println("Test NG started !!!! ");
 
-        // WebDriverManager setup
-        WebDriverManager.firefoxdriver().setup(); // Automatically sets up FirefoxDriver
-
-        driver = new FirefoxDriver();
-    }
-    @Test
-    public void navigate() {
+		String projectLocation = System.getProperty("user.dir");
+		System.out.println(projectLocation);
+		System.setProperty("webdriver.gecko.driver",projectLocation + "/src/lib/geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+	@Test
+	public void navigate() {
         System.out.println("Test NG in progress 1 !!!! ");
 
-        JavascriptExecutor js = (JavascriptExecutor)driver;    
-        driver.get("https://www.paruvendu.fr");
-        js.executeScript("cmp_pv.cookie.saveConsent(true);");
-         try {
-                Thread.sleep(2000); 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }       
-        
-    driver.navigate().refresh();
-          try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-    
-          driver.findElement(By.partialLinkText("Déposer une annonce gratuite")).click();
-          try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-          driver.findElement(By.xpath("//a[contains(@class, '_2hYQUYBGPVPPF8rtNxLrLI') and text()='Plus tard']")).click();
-              
-    
+		JavascriptExecutor js = (JavascriptExecutor)driver;	
+		driver.get("https://www.paruvendu.fr");
+		js.executeScript("cmp_pv.cookie.saveConsent(true);");
+		 try {
+	            Thread.sleep(2000); 
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }		
+		
+		
+	driver.navigate().refresh();
+		  try {
+	            Thread.sleep(2000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	
+		  driver.findElement(By.partialLinkText("Déposer une annonce gratuite")).click();
+		  try {
+	            Thread.sleep(2000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+		  driver.findElement(By.xpath("//a[contains(@class, '_2hYQUYBGPVPPF8rtNxLrLI') and text()='Plus tard']")).click();
+		 		
+	
+	
+	
 
+		  WebElement  dropdown  = driver.findElement(By.cssSelector("div.select"));
+		  dropdown.click();
+		  try {
+	            Thread.sleep(2000); // Wait for 2 seconds
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+		  WebElement autoMotoBateau = driver.findElement(By.cssSelector("li[data-value='V']"));
+		  autoMotoBateau.click();
+		  try {
+	            Thread.sleep(2000); // Wait for 2 seconds
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+		  WebElement voitureOccasion = driver.findElement(By.cssSelector("li[data-value='VVO00000']"));
+		  voitureOccasion.click();
 
-          WebElement  dropdown  = driver.findElement(By.cssSelector("div.select"));
-          dropdown.click();
-          try {
-                Thread.sleep(2000); // Wait for 2 seconds
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-          WebElement autoMotoBateau = driver.findElement(By.cssSelector("li[data-value='V']"));
-          autoMotoBateau.click();
-          try {
-                Thread.sleep(2000); // Wait for 2 seconds
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-          WebElement voitureOccasion = driver.findElement(By.cssSelector("li[data-value='VVO00000']"));
-          voitureOccasion.click();
+		  Select categ = new Select(driver.findElement(By.id("choixFamille")));
+		  categ.selectByVisibleText("Location");
+		  
+		  Select sousRubrique = new Select(driver.findElement(By.id("categorie")));
+		  sousRubrique.selectByVisibleText("Voiture");
+		  		  
+		  driver.findElement(By.id("descBien")).sendKeys("ceci est la description de houcem");
+		  
+		  
+		  Select diffusionAnnonce = new Select(driver.findElement(By.id("nbrSemainesPublication")));
+		  diffusionAnnonce.selectByVisibleText("Pendant 6 mois");
+		  
+		  
+		  driver.findElement(By.id("prix")).sendKeys("25");
+		  try {
+	            Thread.sleep(2000); 
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+		  driver.findElement(By.id("codePostal")).sendKeys("75013");
+		  try {
+	            Thread.sleep(2000); 
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+		  
+		  driver.findElement(By.id("suivant1")).click();
+		  String codeSource =  driver.getPageSource();
+		  String chaine ="Vos coordonnées de contact";
+		  
+		  if (codeSource.contains(chaine)){
+			  System.out.println("existe");
+		  }else {
+			  System.out.println("n''existe pas");
 
-          Select categ = new Select(driver.findElement(By.id("choixFamille")));
-          categ.selectByVisibleText("Location");
-          
-          Select sousRubrique = new Select(driver.findElement(By.id("categorie")));
-          sousRubrique.selectByVisibleText("Voiture");
-                  
-          driver.findElement(By.id("descBien")).sendKeys("ceci est la description de houcem");
-          
-          
-          Select diffusionAnnonce = new Select(driver.findElement(By.id("nbrSemainesPublication")));
-          diffusionAnnonce.selectByVisibleText("Pendant 6 mois");
-          
-          
-          driver.findElement(By.id("prix")).sendKeys("25");
-          try {
-                Thread.sleep(2000); 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-          driver.findElement(By.id("codePostal")).sendKeys("75013");
-          try {
-                Thread.sleep(2000); 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-          
-          driver.findElement(By.id("suivant1")).click();
-          String codeSource =  driver.getPageSource();
-          String chaine ="Vos coordonnées de contact";
-          
-          if (codeSource.contains(chaine)){
-              System.out.println("existe");
-          }else {
-              System.out.println("n''existe pas");
+		  }
+		  				
+	        System.out.println("Test NG in progress 2 !!!! ");
 
-          }
-                    
-
-            System.out.println("Test NG in progress 2 !!!! ");
-
-    }
-    
-    @AfterTest
-    public void tearDown() {
-        
-        
+	
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		
+		
         System.out.println("Test NG completed successfully !!!! ");
         //driver.close();
         //driver.quit();
-    }
+	}
 
 }
